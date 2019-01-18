62a2a21a32903ba630710894b7ef70ec1b684600
Logan <lobsterlogan43@yahoo.com>
2019-01-17 22:18:16 -0600

    1

---
+++ freezegun/gamemodes/terrortown/entities/weapons/weapon_ttt_windowsfreezegun.lua
@@ -0,0 +1,139 @@
+--// A lot of the gun shit I ripped right from the freeze gun, since it suited my purposes quite nicely
+AddCSLuaFile()
+
+SWEP.HoldType              = "pistol"
+
+if CLIENT then
+   SWEP.PrintName          = "Logan's Freeze Gun"
+   SWEP.Slot               = 6
+
+   SWEP.ViewModelFOV       = 54
+   SWEP.ViewModelFlip      = false
+
+   SWEP.EquipMenuData = {
+      type = "a",
+      desc = "Shoot a terrorist to freeze their screen.\nRandomly plays one of three overlays."
+   };
+
+   SWEP.Icon               = "vgui/ttt/" --vgui/ttt/icon_flare
+end
+
+SWEP.Base                  = "weapon_tttbase"
+
+-- if I run out of ammo types, this weapon is one I could move to a custom ammo
+-- handling strategy, because you never need to pick up ammo for it
+SWEP.Primary.Ammo          = "AR2AltFire"
+SWEP.Primary.Recoil        = 2
+SWEP.Primary.Damage        = 1
+SWEP.Primary.Delay         = 1.0
+SWEP.Primary.Cone          = 0.01
+SWEP.Primary.ClipSize      = 3
+SWEP.Primary.Automatic     = false
+SWEP.Primary.DefaultClip   = 3
+SWEP.Primary.ClipMax       = 3
+SWEP.Primary.Sound         = Sound( "Weapon_USP.SilencedShot" )
+
+SWEP.Kind                  = WEAPON_EQUIP
+SWEP.CanBuy                = {ROLE_TRAITOR, ROLE_DETECTIVE} -- only traitors can buy
+SWEP.LimitedStock          = false -- only buyable once
+--SWEP.WeaponID              = AMMO_FLARE
+
+SWEP.Tracer                = "AR2Tracer"
+
+SWEP.UseHands              = true
+SWEP.ViewModel             = Model("models/weapons/c_357.mdl")
+SWEP.WorldModel            = Model("models/weapons/w_357.mdl")
+
+function FreezeTarget( att, path, dmginfo )
+    if SERVER then
+        local ent = path.Entity
+        if not IsValid(ent) then return end
+
+        -- disallow if prep or post round
+        if not GAMEMODE:AllowPVP() then return end 
+
+        if ent:IsPlayer() then
+            local savedRandomNumber
+            ent:Freeze( true )
+
+            local function UnFreeze( ply )
+                ply:Freeze( false )
+                ply:SetDSP( 0, false )
+                net.Start( "EndScreen" )
+                net.Send( ply )
+            end
+
+            timer.Simple( GetConVar( "FreezeGunTime" ):GetInt(), UnFreeze( ent ) ) --TO TEST "UnFreeze( ent )" function syntax
+
+            if GetConVar( "LFG_EnableDynamicFreezing" ):GetInt() == 1 then
+                local ConVarTable = {}
+                if GetConVar( "LFG_EnableIceOverlay" ):GetInt() == 1 then
+                    ConVarTable[ #ConVarTable + 1 ] = "LFG_EnableIceOverlay"
+                end
+                if GetConVar( "LFG_EnableOSUpdateOverlay" ):GetInt() == 1 then
+                    ConVarTable[ #ConVarTable + 1 ] = "LFG_EnableOSUpdateOverlay"
+                end
+                if GetConVar( "LFG_EnableOSCrashOverlay" ):GetInt() == 1 then
+                    ConVarTable[ #ConVarTable + 1 ] = "LFG_EnableOSCrashOverlay"
+                end
+                if GetConVar( "LFG_EnableGameCrashOverlay" ):GetInt() == 1 then
+                    ConVarTable[ #ConVarTable + 1 ] = "LFG_EnableGameCrashOverlay"
+                end
+
+                net.Start( "SendScreenInteractive" )
+                savedRandomNumber = math.random( -254, 254 )
+                net.WriteInt( savedRandomNumber, 9 ) --Used as verification
+                net.Writestring( ConVarTable[ math.random( #ConVarTable ) ] ) --Random overlay to use
+            else
+                net.Start( "SendScreen" )
+                ent:SetDSP( 4, false )
+            end
+            net.Send( ent )
+
+            net.Receive( "SendScreenInteractiveCallback", function( len, ply )
+                local test = net.ReadInt( 9 )
+                if test != savedRandomNumber then return end --Never trust the client
+
+                UnFreeze( ply )
+            end )
+        end
+    end
+end
+
+function SWEP:ShootFreeze()
+   local cone = self.Primary.Cone
+   local bullet = {}
+   bullet.Num       = 1
+   bullet.Src       = self:GetOwner():GetShootPos()
+   bullet.Dir       = self:GetOwner():GetAimVector()
+   bullet.Spread    = Vector( cone, cone, 0 )
+   bullet.Tracer    = 1
+   bullet.Force     = 2
+   bullet.Damage    = self.Primary.Damage
+   bullet.TracerName = self.Tracer
+   bullet.Callback = FreezeTarget
+
+   self:GetOwner():FireBullets( bullet )
+end
+
+function SWEP:PrimaryAttack()
+   self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+   if not self:CanPrimaryAttack() then return end
+
+   self:EmitSound( self.Primary.Sound )
+   self:SendWeaponAnim( ACT_VM_PRIMARYATTACK )
+   self:TakePrimaryAmmo( 1 )
+
+   if IsValid(self:GetOwner()) then
+      self:GetOwner():SetAnimation( PLAYER_ATTACK1 )
+
+      self:GetOwner():ViewPunch( Angle( math.Rand(-0.2,-0.1) * self.Primary.Recoil, math.Rand(-0.1,0.1) *self.Primary.Recoil, 0 ) )
+   end
+
+   if ( (game.SinglePlayer() && SERVER) || CLIENT ) then
+      self:SetNWFloat( "LastShootTime", CurTime() )
+   end
+end
+
+function SWEP:SecondaryAttack()
+end
---
+++ freezegun/lua/autorun/freezegun_net.lua
@@ -0,0 +1,148 @@
+if SERVER then
+
+    util.AddNetworkString( "SendScreen" )
+    --util.AddNetworkString( "SendScreenCallback" )
+    util.AddNetworkString( "SendScreenInteractive" )
+    util.AddNetworkString( "SendScreenInteractiveCallback" )
+    util.AddNetworkString( "EndScreen" )
+    --util.AddNetworkString( "EndScreenCallback" )
+
+    CreateConVar( "LFG_EnableIceOverlay", 1, { FCVAR_ARCHIVE }, "If set to 1, enables the interactive ice-over overlay" )
+    CreateConVar( "LFG_EnableOSUpdateOverlay", 1, { FCVAR_ARCHIVE }, "If set to 1, enables the interactive OS update overlay" )
+    CreateConVar( "LFG_EnableOSCrashOverlay", 1, { FCVAR_ARCHIVE }, "If set to 1, enables the interactive OS crash overlay" )
+    CreateConVar( "LFG_EnableGameCrashOverlay", 1, { FCVAR_ARCHIVE }, "If set to 1, enables the interactive game crash overlay" )
+    CreateConVar( "LFG_EnableDynamicFreezing", 1, { FCVAR_ARCHIVE }, "If set to 1, enables the interactive freezing,
+        interactive freezing allows the victim to interact with some puzzle in order to escape being frozen sooner" )
+    CreateConVar( "LFG_FreezeGunTime", 5, { FCVAR_ARCHIVE }, "How long the frozen player remains unable to move" )
+
+end
+
+if CLIENT then
+    local Screen, savedInt
+
+    net.Receive( "SendScreenInteractive", function()
+        savedInt = net.ReadInt( 9 )
+        local overlayType = net.ReadString()
+
+        --Ugly-ass if statement, but necessary since I have additional functionality dependent on the option
+        if system.IsLinux or system.IsOSX then
+            --say something here about being lame
+            DefaultOverlay()
+        elseif overlayType == "LFG_EnableIceOverlay" then --Ice overlay
+                IceInteractiveOverlay()
+        elseif overlayType == "LFG_EnableOSUpdateOverlay" then
+            OSUpdateInteractiveOverlay()
+        elseif overlayType == "LFG_EnableOSCrashOverlay" then
+            OSCrashInteractiveOverlay()
+        elseif overlayType == "LFG_EnableGameCrashOverlay" then
+            GameCrashInteractiveOverlay()
+        else
+            DefaultOverlay()
+        end
+
+    end )
+
+    net.Receive( "SendScreen", DefaultOverlay )
+
+    net.Receive( "EndScreen", CloseOverlay ) --All this should do is close the screen, everything else should be done for us
+
+    function MuteThem()
+        for k, v in pairs( player.GetAll() ) do
+            v:SetMuted( true )
+        end
+    end
+
+    function UnMuteThem()
+        for k, v in pairs( player.GetAll() ) do
+            if v:Alive() then
+                v:SetMuted( false )
+            end
+        end
+    end
+
+    function DefaultOverlay()
+        Screen = vgui.Create( "DFrame" )
+        Screen:SetSize( ScrW(), ScrH() )
+        Screen:SetPos( 0, 0 )
+        Screen:SetTitle( "" )
+        Screen:SetVisible( true )
+        Screen:SetDraggable( false )
+        Screen:ShowCloseButton( false )
+        Derma_DrawBackgroundBlur( Screen, CurTime() ) --Is this desired?
+
+        local IceOverlay = Material( "" ) --Smooth 1
+        overlayPanel = vgui.Create( "DPanel", Screen )
+        overlayPanel:SetSize( Screen:GetWide(), Screen:GetTall() )
+        overlayPanel:SetPos( 0, 0 )
+        overlayPanel.Paint = function()
+            --Draw icy screen overlay here
+            surface.SetMaterial( IceOverlay )
+            surface.DrawTexturedRect( 0, 0, overlayPanel:GetWide(), overlayPanel:GetTall() )
+        end
+    end
+
+    function CloseOverlay()
+        Screen:Close()
+        UnMuteThem()
+    end
+
+    function CompletedMinigame()
+        net.Start( "SendScreenInteractiveCallback" ) --To be called only when the player SUCCESSFULLY completes the minigame
+            net.WriteInt( savedInt, 9 )
+        net.SendToServer()
+    end
+
+    function IceInteractiveOverlay()
+        LocalPlayer():SetDSP( 4, false )
+
+        Screen = vgui.Create( "DFrame" )
+        Screen:SetSize( ScrW(), ScrH() )
+        Screen:SetPos( 0, 0 )
+        Screen:SetTitle( "" )
+        Screen:SetVisible( true )
+        Screen:SetDraggable( false )
+        Screen:ShowCloseButton( false )
+        Screen:MakePopup()
+        Derma_DrawBackgroundBlur( Screen, CurTime() ) --Is this desired?
+
+        local IceOverlay = Material( "" ) --Smooth 1
+        overlayPanel = vgui.Create( "DPanel", Screen )
+        overlayPanel:SetSize( Screen:GetWide(), Screen:GetTall() )
+        overlayPanel:SetPos( 0, 0 )
+        overlayPanel.Paint = function()
+            --Draw icy screen overlay here
+            surface.SetMaterial( IceOverlay )
+            surface.DrawTexturedRect( 0, 0, overlayPanel:GetWide(), overlayPanel:GetTall() )
+        end
+    end
+
+    function OSUpdateInteractiveOverlay()
+        --No screen effect except a small window at the bottom of the screen asking for windows to update to, must click through multiple windows
+        Screen = vgui.Create( "DFrame" )
+        Screen:SetSize( ScrW(), ScrH() )
+        Screen:SetPos( 0, 0 )
+        Screen:SetTitle( "" )
+        Screen:SetVisible( true )
+        Screen:SetDraggable( false )
+        Screen:ShowCloseButton( false )
+        Screen:MakePopup()
+        Derma_DrawBackgroundBlur( Screen, CurTime() ) --Is this desired?
+        
+        overlayPanel = vgui.Create( "DPanel", Screen )
+        overlayPanel:SetSize( Screen:GetWide(), Screen:GetTall() )
+        overlayPanel:SetPos( 0, 0 )
+        overlayPanel.Paint = function()
+            --Make a Windows update screen here
+        end
+    end
+
+    function OSCrashInteractiveOverlay()
+        MuteThem()
+        --Blue screen text-based minigame
+    end
+
+    function GameCrashInteractiveOverlay()
+        MuteThem()
+        --Game freezes, have to time space bar presses with the green bar crossing a line, gets continually harder as you succeed
+    end
+end
